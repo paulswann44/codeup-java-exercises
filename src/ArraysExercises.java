8e798627d2cc6178d9566b2ceab07004f6ecfbf9
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-12-22 20:11:54 -0600

    holiday work

---
+++ src/ArraysExercises.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    //a-1
+//This way works just fine.
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+//        RESULTS:
+//        1
+//        2
+//        3
+//        4
+//        5
+
+//a-2
+//use What happens when you run the following code? Why is Arrays.toString necessary?
+//        public static void main(String[] args) {
+        System.out.println(Arrays.toString(numbers)); // RESULTS: [1, 2, 3, 4, 5]
+
+//a-3
+//Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
+        String[] persons = {"Paul", "David", "Sam"};
+        String[] newPerson = new String[persons.length + 1];
+        for (String person : persons) {
+            System.out.println(person);
+//            RESULTS:
+//            Paul
+//            David
+//            Sam
+        }
+
+    }
+
+    //a-4
+    //Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+    public static String[] addPerson(String[] person, String newName) {
+        //creating a new String object with an increase in length size
+        String[] newPersons = new String[person.length + 1];
+        //Copy it by a loop
+        for (int i = 0; i < person.length; i++) {
+            newPersons[i] = person[i];
+        }
+
+        newPersons[newPersons.length - 1]= newName;
+        return newPersons;
+
+    }
+}
+
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,13 +1,41 @@
-package util;
-import java.util.Scanner;
-
-public class Input {
-    private Scanner scanner{
-
-        public Input(){
-            this.scanner = new Scanner(System.in);
-
-        }
-
-    }
-}
+//package util;
+//
+////    (1) Create a package inside of src named util.
+//
+//import java.util.Scanner;
+//
+////    (2) Inside of util, create a class named Input that has a private field named scanner.
+//public class Input {
+//    //This is encapsulated the field hence private.  It prevents alteration of the scanner class.
+//    private Scanner scanner;
+//
+//    //    (3) When an instance of this object is created, the scanner field should be set to a new instance of the Scanner class.
+//    public Input() {
+//        System.out.println("try something");
+//        this.scanner = new Scanner(System.in); //since it is an instance,
+//    }
+////    (4) The class should have the following methods, all of which return command line input from the user:
+//
+//     String getString(){
+//        return scanner.nextLine();
+//    }
+//    boolean yesNo(){
+////        public String getString(){
+////        }
+////        }
+////    (c) int getInt(int min, int max)
+////    (d) int getInt()
+////    (e) double getDouble(double min, double max)
+////    (f) double getDouble()
+//
+////    public static void main(String[] args){
+//
+//    }
+//
+//}
+//
+//
+//
+//
+//
+//
